c2b27004646c82849440358206d5a0597643aacb
Vuyani Magibisela <vuyani.magibisela@live.com>
2023-01-24 13:59:20 +0200

    Add login and sign up systems

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!--Google analytics
+	================================================-->
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-156064280-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'UA-156064280-1');
+	</script>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/form_reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Home</title>
+</head>
+<body>
+    <header>
+        <nav id="sidebar-container" class="sidebar">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                <a href="index.html" class="active">Home</a>
+                <a href="projectuplod.html" >Project Upload </a>
+                <a href="resources.html">Resources</a>
+                <a href="about.html">About</a>
+        </nav>
+        
+        <button class="openbtn" onclick="openNav()">&#9776; </button>
+            
+        <img src="img/Sci-Bono logo White.png" alt="" class="sblogo">
+        <h1 class="title">TVET Instructor Training</h1>
+        <div class="regBtns">
+            <div class="signup">
+                <a href="registration.php"><button class="button-9" role="button">Sign Up</button></a>
+            </div>
+            
+            <div class="login">
+                <a href="login.php"><button class="button-23" role="button">Log In</button></a>
+            </div>
+        </div>
+       
+    </header>
+
+    <div id="main-container">
+        <div class="hero">
+            <img src="img/hero.png" alt="">
+        </div>
+        <div class="intro">
+        <p>
+            <span>Welcome to Sci-Bono</span><br>
+            We provide training programs to equip lecturers 
+            with digital literacy skills to develop a digitally 
+            competent workforce. Join us in our mission to 
+            improve the lives of South Africans through 
+            education and training.
+        </p>
+        </div>
+        <div class="program">
+            <h2>
+                Our Training Program
+            </h2>
+            <div class="text-row1">
+                <p class="text">
+                    Our proposed training program aims to provide lecturers with digital literacy and e-teaching skills to develop a digitally competent workforce capable of adapting to technological changes.
+                </p>
+               
+            </div>
+
+
+            <div class="text-row2">
+                <p class="text">
+                    Our training program will help lecturers gain knowledge and skills to create engaging online learning environments, enhance the learning experience and effectively integrate digital tools into their teaching practice, tailored to the needs of TVET lecturers
+                </p>
+                
+            </div>
+            <div class="text-row3">
+                <p class="text">
+                    Our training program is delivered by experienced 
+                    trainers and we provide high-quality 
+                    support to ensure lecturers get the 
+                    most out of the program and 
+                    continue to improve their digital 
+                    literacy skills.
+                </p>
+            </div>
+
+            <div class="image-row1">
+                <img src="img/teacher.png" alt=""> 
+            </div>
+            <div class="image-row2">
+                <img src="img/skilled.png" alt="">
+            </div>
+            <div class="image-row3">
+                <img src="img/competent.png" alt="">
+            </div>
+            
+            </div>
+
+            <div class="modules">
+                <h2>
+                    Training Modules
+                </h2>
+                <div class="moduleCards">
+                    <div class="dropdown">
+                        <iconify-icon class="iconDropdown" icon="ic:round-arrow-drop-down-circle" width="20px"></iconify-icon>
+                        <h1>Digital Literacy</h1>
+                    </div>
+                    <p>This module will cover the basics of digital literacy including computer hardware and software, internet navigation, and digital communication tools.</p>
+                </div>
+                <div class="moduleCards">
+                    <div class="dropdown">
+                        <iconify-icon class="iconDropdown" icon="ic:round-arrow-drop-down-circle" width="20px"></iconify-icon>
+                        <h1>E-teaching and Digital Pedagogy</h1>
+                    </div>
+                    <p>This module covers the principles of e-teaching and how to integrate digital tools into the curriculum.</p>
+                </div>
+                <div class="moduleCards">
+                    <div class="dropdown">
+                        <iconify-icon class="iconDropdown" icon="ic:round-arrow-drop-down-circle" width="20px"></iconify-icon>
+                        <h1>Interactive Curriculum Delivery</h1>
+                    </div>
+                    <p>This module covers how to use digital tools to assess and evaluate student learning.</p>
+                </div>
+                <div class="moduleCards">
+                    <div class="dropdown">
+                        <iconify-icon class="iconDropdown" icon="ic:round-arrow-drop-down-circle" width="20px"></iconify-icon>
+                        <h1>Digital Assessment and Evaluation</h1>
+                    </div>
+                    <p>This module covers how to use digital tools to assess and evaluate student learning.</p>
+                </div>
+                <div class="moduleCards">
+                    <div class="dropdown">
+                        <iconify-icon class="iconDropdown" icon="ic:round-arrow-drop-down-circle" width="20px"></iconify-icon>
+                        <h1>Digital Media and Communication</h1>
+                    </div>
+                    <p> This module covers how to use digital media and communication tools to enhance the learning experience and create a sense of community among students.</p>
+                </div>
+                <div class="moduleCards">
+                    <div class="dropdown">
+                        <iconify-icon class="iconDropdown" icon="ic:round-arrow-drop-down-circle" width="20px"></iconify-icon>
+                        <h1>Advanced Digital Skills</h1>
+                    </div>
+                    <p> This module covers more advanced topics such as data analysis, programming, and digital security.</p>
+                </div>
+                <div class="moduleOutro">
+                <p>
+                    Each module is designed to provide relevant and hands-on training for the lecturers, with the goal of giving them the skills and knowledge to effectively integrate technology into their teaching practice, and to provide their students with a high-quality education that prepares them for the digital age.
+                </p>
+            </div>
+
+            </div>
+        </div>
+
+        
+    </div>
+    
+    <script src="js/script.js"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
+</body>
+</html>
